ed518af447517f74877ce0658942c2d5d83ccc41
TLAMHutto <keatonhutto8@gmail.com>
2022-06-23 22:47:43 -0500

    stuff

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "antd/dist/antd.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-// 1. import from react-redux and redux
+
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
@@ -26,7 +25,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
