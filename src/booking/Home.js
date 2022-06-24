47a341d042fcded0e986c6ff9c427bae391fcd91
TLAMHutto <keatonhutto8@gmail.com>
2022-06-23 19:49:37 -0500

    stuff

--- src/booking/Home.js
+++ src/booking/Home.js
@@ -1,10 +1,12 @@
-import { useSelector, useStore } from 'react-redux'
+import { useSelector } from "react-redux";
 
 const Home = () => {
-  const state = useSelector((state) => state);
+  const { user } = useSelector((state) => ({ ...state }));
   return (
-    
-    <div className="container-fluid h1 p-5 text-center">Home {JSON.stringify(state)}</div>
-  )
-}
-export default Home
+    <div className="container-fluid h1 p-5 text-center">
+      Home Page {JSON.stringify(user)}
+    </div>
+  );
+};
+
+export default Home;
